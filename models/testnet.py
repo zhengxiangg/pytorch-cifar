3b5939d82ac04d3e80bb1d766ef30761613294b2
zx <2646125453@qq.com>
2021-09-27 11:23:23 +0800

    add a one FC layer as testnet

--- models/testnet.py
+++ models/testnet.py
@@ -2,9 +2,16 @@ import torch.nn as nn
 
 class testNet(nn.Module):
     def __init__(self):
-        self.layer = nn.Linear(32*32, 10)
-        self.m = nn.Softmax(dim=10)
+        self.layer = nn.Sequential(
+            nn.Linear(32 * 32, 1000)
+            nn.Sigmoid()
+            nn.Linear(1000,1000)
+            nn.Sigmoid()
+            nn.Linear(1000,10)
+            nn.Softmax(dim=10)
+        )
+
+
     def forward(self,x):
         out = self.layer(x)
-        out = self.m(out)
-        retrun out
+        return out
